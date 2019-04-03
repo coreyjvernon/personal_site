25048a1baa2c9aa9d0076243e647b86374c26efe
Corey Vernon <coreyjvernon@gmail.com>
2019-04-03 12:29:25 -0400

    initial commit for personal page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header/header';
+import About from './components/About/about';
+import Resume from './components/Resume/resume';
+import Projects from  './components/Projects/projects';
+import Portfolio from './components/Portfolio/portfolio';
+import Contact from './components/Contact/contact';
+import Footer from './components/Footer/footer';
+import resumeData from './resumeData';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header resumeData={resumeData}/>
+        <About />
+        <Resume />
+        <Projects />
+        <Portfolio />
+        <Contact />
+        <Footer />
       </div>
     );
   }
